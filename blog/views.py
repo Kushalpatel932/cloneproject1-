42128d6a0ee51ffa5b29d954466d99c6a7484f76
Kushalpatel932 <kushalpatel932@gmail.com>
2022-08-21 16:20:50 +0530

    'cloneproject'

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from pipes import Template
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect
 from django.views.generic import (TemplateView,ListView,DetailView,CreateView,UpdateView,DeleteView)
 from .models import Post,Comment
 from .forms import PostForm,CommentForm
@@ -52,3 +52,39 @@ class DraftListView(ListView):
     model = Post
     def get_queryset(self):
         return Post.objects.filter(published_date__isnull=True).order_by('created_date')
+
+
+def post_publish(request, pk):
+    #use seesion 
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+
+def add_comment_to_post(request, pk):
+    #session
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/comment_form.html', {'form': form})
+
+def comment_approve(request, pk):
+#session
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+
+def comment_remove(request, pk):
+    #session
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail', pk=post_pk)
